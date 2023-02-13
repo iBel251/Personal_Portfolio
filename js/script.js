1b6c078ac4d822d2fe0d061119c8da8eb933951d
iBel251 <ibbimran7@gmail.com>
2023-02-13 20:32:50 +0300

    Add responsive layouts

--- js/script.js
+++ js/script.js
@@ -15,11 +15,21 @@ const nav = document.querySelector(".nav"),
     for(let i=0; i<totalNavList; i++) {
         const a = navList[i].querySelector("a");
         a.addEventListener("click", function() {
+            for (let i = 0; i < totalSection; i++) {
+                allSection[i].classList.remove("back-section");
+                
+            }
             for(let j=0; j<totalNavList; j++) {
+                if(navList[j].querySelector("a").classList.contains("active")){
+                    allSection[j].classList.add("back-section")
+                }
                 navList[j].querySelector("a").classList.remove("active");
             }
             this.classList.add("active")
             showSection(this);
+            if(window.innerWidth < 1200) {
+                asideSectionTogglerBtn();
+            }
         })
     }
 
@@ -31,3 +41,16 @@ const nav = document.querySelector(".nav"),
         const target = element.getAttribute("href").split("#")[1];
         document.querySelector("#" + target).classList.add("active")
     }
+
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+        aside = document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click", () => {
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn(){
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for(let i=0; i<totalSection; i++) {
+                allSection[i].classList.toggle("open")
+            }
+        }
